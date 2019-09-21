d9b824af0bb634f6adc27d67055d134c68fca5e6
Fahg2541 <Fahg2541@gmail.com>
2019-09-21 16:27:54 +0700

    update Fizzbuzz and Fizzbuzz.test

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,39 @@
 class Fizzbuzz {
     static say(number) {
-        return 1;
+        // if (number % 3 == 0 && number % 5 == 0) {
+        //     return ("Fizzbuzz");
+        // }
+        // else if (number % 3 == 0) {
+        //     return ("Fizz");
+        // }
+        // else if (number % 5 == 0) {
+        //     return ("Buzz");
+        // }
+        // else {
+        //     return number
+        // }
+        // let number = ;
+        switch (true) {
+            case this.mod(number, 15) === 0:
+                return ("Fizzbuzz");
+
+            case this.mod(number, 3) === 0:
+                return ("Fizz");
+
+            case this.mod(number, 5) === 0:
+                return ("Buzz");
+            default:
+                return number;
+
+
+        }
+    }
+
+    static mod(number, base) {
+        // while (number >= base) {
+        //     number -= base;
+        // }
+        return number - Math.floor(number / base) * base;
     }
 }
 module.exports = Fizzbuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,9 +1,16 @@
 const Fizzbuzz = require("./fizzbuzz");
 
 describe("Test Fizzbuzz", () => {
-    test("Test 1 expect 1", () => {
-        const result = Fizzbuzz.say(1);
-        expect(result).toEqual(1) // Assert Function
+    test("Test 3 expect 3", () => {
+        const result = Fizzbuzz.say(3);
+        expect(result).toEqual("Fizz") // Assert Function
 
     });
+    test("Test 5%5 expect 0", () => {
+        const result = Fizzbuzz.mod(5)
+    })
+
+
+
+
 });
